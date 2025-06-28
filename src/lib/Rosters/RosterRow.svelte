05053396dd3293a08c352444248f35e3241e95f6
NickPuts <NBPuts@gmail.com>
2025-06-28 00:33:03 -0400

    Update RosterRow.svelte

--- src/lib/Rosters/RosterRow.svelte
+++ src/lib/Rosters/RosterRow.svelte
@@ -2,149 +2,11 @@
   import { Row, Cell } from '@smui/data-table';
 
   export let player;
+  export let keeperInfo;
 
   const playerSLotClass = player.slot.replace('/', '').replace('SUPER_', 'S-').replace('REC_', 'R-');
   const playerSlot = player.slot.replace('SUPER_', 'S ').replace('REC_', 'R ');
 </script>
-
-<style>
-  .playerAvatar {
-    position: relative;
-    vertical-align: middle;
-    height: 40px;
-    width: 40px;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: auto 45px;
-    overflow: visible;
-  }
-
-  .teamLogo {
-    width: 21px;
-    position: absolute;
-    top: 0;
-    right: -16px;
-  }
-
-  .pos {
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 8px;
-    width: 46px;
-    height: 32px;
-  }
-
-  .QB { background-color: var(--QB); }
-  .WR { background-color: var(--WR); }
-  .RB { background-color: var(--RB); }
-  .TE { background-color: var(--TE); }
-  .K  { background-color: var(--K); }
-  .DEF { background-color: var(--DEF); }
-  .DL { background-color: var(--DL); }
-  .LB { background-color: var(--LB); }
-  .DB { background-color: var(--DB); }
-  .BN { background-color: var(--BN); }
-
-  .FLEX {
-    background: linear-gradient(to right, var(--WR) 0%, var(--WR) 33.33%, var(--RB) 33.33%, var(--RB) 66.66%, var(--TE) 66.66%);
-  }
-
-  .WRRB {
-    background: linear-gradient(to right, var(--WR) 0%, var(--WR) 50%, var(--RB) 50%);
-  }
-
-  .S-FLEX {
-    background: linear-gradient(to right, var(--WR) 0%, var(--WR) 25%, var(--RB) 25%, var(--RB) 50%, var(--TE) 50%, var(--TE) 75%, var(--QB) 75%);
-  }
-
-  .R-FLEX {
-    background: linear-gradient(to right, var(--WR) 0%, var(--WR) 50%, var(--TE) 50%);
-  }
-
-  .IDP {
-    background: linear-gradient(to right, var(--DL) 0%, var(--DL) 33.33%, var(--LB) 33.33%, var(--LB) 66.66%, var(--DB) 66.66%);
-  }
-
-  :global(.slot) {
-    width: 22px !important;
-    padding: 1px 6px 1px 8px !important;
-  }
-
-  :global(.avatar) {
-    width: 45px;
-  }
-
-  :global(.playerCell) {
-    height: 45px !important;
-    overflow-y: hidden !important;
-  }
-
-  :global(.nameCell) {
-    padding: 0 5px;
-  }
-
-  span.nickname {
-    color: #888;
-    font-style: italic;
-    font-size: 0.8em;
-  }
-
-  :global(.injury) {
-    font-style: italic;
-    font-weight: 700;
-    font-size: 0.7em;
-    margin-left: 0.5em;
-    vertical-align: super;
-  }
-
-  :global(.Q) { color: #c09111; }
-  :global(.OUT),
-  :global(.IR),
-  :global(.PUP) { color: #851313; }
-
-  .additionalInfo {
-    display: inline-block;
-    font-size: 0.7em;
-  }
-
-  .text-QB  { color: var(--QB); }
-  .text-WR  { color: var(--WR); }
-  .text-RB  { color: var(--RB); }
-  .text-TE  { color: var(--TE); }
-  .text-K   { color: var(--K); }
-  .text-DEF { color: var(--DEF); }
-  .text-DL  { color: var(--DL); }
-  .text-LB  { color: var(--LB); }
-  .text-DB  { color: var(--DB); }
-
-  @media (max-width: 460px) {
-    :global(.nameCell) {
-      font-size: 0.9em;
-    }
-  }
-
-  @media (max-width: 355px) {
-    :global(.slot) {
-      width: 22px !important;
-      padding: 1px 2px 1px 2px !important;
-    }
-
-    :global(.avatar) {
-      padding: 0px 16px 0px 2px !important;
-    }
-
-    .pos {
-      width: 42px;
-      height: 24px;
-      font-size: 0.8em;
-    }
-
-    :global(.nameCell) {
-      font-size: 0.85em;
-    }
-  }
-</style>
 
 <Row>
   <Cell class="slot playerCell">
@@ -180,5 +42,24 @@
     {#if player.nickname}
       <span class="nickname">"{player.nickname}"</span>
     {/if}
+
+    <!-- Keeper Info -->
+    {#if keeperInfo}
+      <div class="additionalInfo keeper">
+        Keeper: {keeperInfo.eligible ? keeperInfo.round : 'X'}
+        &nbsp;|&nbsp;
+        YOS: {keeperInfo.years}
+      </div>
+    {/if}
   </Cell>
 </Row>
+
+<style>
+  /* (keep your existing styles unchanged) */
+
+  .keeper {
+    margin-top: 3px;
+    font-size: 0.75em;
+    color: #888;
+  }
+</style>
