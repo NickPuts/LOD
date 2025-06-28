7633da24723ef6e692ebc591d0cd402f7f9bd8cb
NickPuts <NBPuts@gmail.com>
2025-06-28 10:16:37 -0400

    Update rosters.svelte

--- src/lib/Rosters/rosters.svelte
+++ src/lib/Rosters/rosters.svelte
@@ -1,17 +1,20 @@
 <script>
 	import { loadPlayers } from '$lib/utils/helper';
-	import RosterSorter from './RosterSorter.svelte'
+	import RosterSorter from './RosterSorter.svelte';
 
-	export let leagueData, rosterData, leagueTeamManagers, playersInfo;
+	export let leagueData;
+	export let rosterData;
+	export let leagueTeamManagers;
+	export let playersInfo;
 
-	let players = playersInfo.players;
+	let players = playersInfo?.players ?? {};
 
 	const refreshPlayers = async () => {
 		const newPlayersInfo = await loadPlayers(null, true);
 		players = newPlayersInfo.players;
-	}
+	};
 
-	if(playersInfo.stale) {
+	if (playersInfo?.stale) {
 		refreshPlayers();
 	}
 </script>
@@ -21,8 +24,23 @@
 		position: relative;
 		z-index: 1;
 	}
+	.error-message {
+		text-align: center;
+		color: red;
+		margin-top: 2em;
+	}
 </style>
 
-<div class="rosters">
-	<RosterSorter rosters={rosterData.rosters} {players} {leagueTeamManagers} startersAndReserve={rosterData.startersAndReserve} {leagueData} />
-</div>
+{#if leagueData && rosterData?.rosters && leagueTeamManagers && Object.keys(players).length > 0}
+	<div class="rosters">
+		<RosterSorter
+			rosters={rosterData.rosters}
+			{players}
+			{leagueTeamManagers}
+			startersAndReserve={rosterData.startersAndReserve}
+			{leagueData}
+		/>
+	</div>
+{:else}
+	<p class="error-message">No rosters to display. Please check your league data or try refreshing.</p>
+{/if}
