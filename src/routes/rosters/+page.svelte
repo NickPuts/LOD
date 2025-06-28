cc7df79cd0ae9c4712350c41058919c1c04bd4af
NickPuts <NBPuts@gmail.com>
2025-06-28 10:34:29 -0400

    Update +page.svelte

--- src/routes/rosters/+page.svelte
+++ src/routes/rosters/+page.svelte
@@ -1,28 +1,24 @@
 <script>
-<script>
   import LinearProgress from '@smui/linear-progress';
-  import Rosters from '$lib/Rosters/rosters.svelte';
+  import Rosters from '$lib/Rosters/Rosters.svelte';
 
   export let data;
   const rostersInfo = data.rostersInfo;
 
-  console.log("Rosters page loaded:", rostersInfo); // 👈 ADD THIS
-</script>
-
-  import LinearProgress from '@smui/linear-progress';
-  import Rosters from '$lib/Rosters/rosters.svelte';
-
-  export let data;
-  const rostersInfo = data.rostersInfo;
+  console.log('ROSTERS INFO:', rostersInfo); // 👈 Add this
 </script>
 
 {#if !rostersInfo}
   <LinearProgress indeterminate />
 {:else}
-  <Rosters
-    leagueData={rostersInfo.leagueData}
-    rosterData={rostersInfo.rosterData}
-    leagueTeamManagers={rostersInfo.leagueTeamManagers}
-    playersInfo={rostersInfo.playersInfo}
-  />
+  {#if rostersInfo.rosterData?.rosters && Object.keys(rostersInfo.rosterData.rosters).length > 0}
+    <Rosters
+      leagueData={rostersInfo.leagueData}
+      rosterData={rostersInfo.rosterData}
+      leagueTeamManagers={rostersInfo.leagueTeamManagers}
+      playersInfo={rostersInfo.playersInfo}
+    />
+  {:else}
+    <p style="text-align: center;">🚨 No rosters to display.</p>
+  {/if}
 {/if}
